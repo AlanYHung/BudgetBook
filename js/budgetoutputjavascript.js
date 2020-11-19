c94f3d78f9b2cf32aa601bab28d560df4d56ba3d
nickmagruder <nick.magruder@outlook.com>
2020-11-18 21:08:07 -0800

    imported local storage code from app.js to summary page js, changed image on index

--- js/budgetoutputjavascript.js
+++ js/budgetoutputjavascript.js
@@ -11,11 +11,101 @@ var summaryCategoryObjectArray = []; */
 /* var insertParent = document.getElementById('summary-table'); */
 
 
-// /////////////////////////////
-// DUMMY DATA SAMPLES
-// From here through line 68
-// /////////////////////////////
-function IncomeObject (ffiCategory, ffiAmount,) {
+// ////////////////////////////////////////////////////////////////////
+// Below section copied from app.js 11/18 at 8:19pm
+
+// income parameters: category, amount, frequency, startingdate
+// io = incomeObject
+// ffi = income form field input
+
+function IncomeObject (ffiCategory, ffiAmount) {
+  this.ioCategory = ffiCategory;
+  this.ioAmount = ffiAmount;
+
+  incomeObjectArray.push(this);
+}
+
+// eo = expenseObject
+// ffe = expense form field input
+
+function ExpenseObject (ffeCategory, ffeAmount, ffeRecurring, ffeTransactionDate, ffeDescription = '') {
+  this.eoCategory = ffeCategory;
+  this.eoAmount = ffeAmount;
+  this.eoRecurring = ffeRecurring; // boolean 
+  this.eoTransactionDate = ffeTransactionDate;
+  this.eoDescription = ffeDescription;
+
+  expenseObjectArray.push(this);
+}
+
+// ffc = form field category
+function CategoryObject (ffcName) {
+  this.categoryName = ffcName;
+  this.categoryBudget = 0;
+
+  categoryObjectArray.push(this);
+}
+
+function defaultCategoryCreator() {
+  new CategoryObject ('Housing');
+  new CategoryObject ('Food');
+  new CategoryObject ('Utilities');
+  new CategoryObject ('Loans');
+  new CategoryObject ('Miscellaneous');
+
+  return categoryObjectArray;
+}
+
+function retrieveObjectsFromLS(){
+  var incomeObjectsFromLS = localStorage.getItem('income');
+  var expenseObjectsFromLS = localStorage.getItem('expense');
+  var categoryObjectsFromLS = localStorage.getItem('category');
+
+  var parsedIncomeObjectArray = JSON.parse(incomeObjectsFromLS);
+  var parsedExpenseObjectArray = JSON.parse(expenseObjectsFromLS);
+  var parsedCategoryObjectArray = JSON.parse(categoryObjectsFromLS);
+
+  generateNewObjectArrays(parsedIncomeObjectArray, parsedExpenseObjectArray, parsedCategoryObjectArray);
+}
+
+function generateNewObjectArrays(income, expense, category){
+  incomeObjectArray = [];
+  expenseObjectArray = [];
+  categoryObjectArray = [];
+
+  for (var incomeCounter=0; incomeCounter < income.length; incomeCounter++){
+    new IncomeObject(income[incomeCounter].ioCategory, income[incomeCounter].ioAmount);
+  }
+
+  for (var expenseCounter=0; expenseCounter < expense.length; expenseCounter++){
+    new ExpenseObject(expense[expenseCounter].eoAmount, expense[expenseCounter].eoRecurring, expense[expenseCounter].eoTransactionDate, expense[expenseCounter].eoDescription, expense[expenseCounter].eoBudget);
+  }
+
+  for (var categoryCounter=0; categoryCounter < category.length; categoryCounter++) {
+    new CategoryObject(category[categoryCounter].categoryName, category[categoryCounter].categoryType, category[categoryCounter].categoryPaymentTrackingStyle);
+  }
+
+}
+
+//Additional LS Functions AYH
+function retrieveUserNameFromLS(){
+  var userNameFromLS = localStorage.getItem('lsUserName');
+
+  return JSON.parse(userNameFromLS);
+}
+
+
+// Above section copied from app.js 11/18 at 8:19pm
+// ////////////////////////////////////////////////////////////////////
+
+/* defaultCategoryCreator()
+retrieveObjectsFromLS() */
+
+// ////////////////////////////////////////////////////////////////////
+// Below section: DUMMY DATA SAMPLES
+
+
+/* function IncomeObject (ffiCategory, ffiAmount,) {
   this.ioCategory = ffiCategory;
   this.ioAmount = ffiAmount;
 
@@ -23,35 +113,40 @@ function IncomeObject (ffiCategory, ffiAmount,) {
 }
 
 new IncomeObject ('Income', 5000);
-new IncomeObject ('Income', 400);
+new IncomeObject ('Income', 400); */
+
+
 
 // eo = expenseObject
 // ffe = expense form field input
-function ExpenseObject (ffeCategory, ffeAmount, ffeRecurring, ffeTransactionDate, ffeBudget, ffeDescription = '') {
+
+
+/* function ExpenseObject (ffeCategory, ffeAmount, ffeRecurring, ffeTransactionDate, ffeBudget, ffeDescription = '') {
   this.eoCategory = ffeCategory;
   this.eoAmount = ffeAmount;
-  this.eoRecurring = ffeRecurring; // boolean 
+  this.eoRecurring = ffeRecurring;
   this.eoTransactionDate = ffeTransactionDate;
   this.eoDescription = ffeDescription;
   this.eoBudget = ffeBudget;
 
   expenseObjectArray.push(this);
-}
+} */
 
-new ExpenseObject ('Housing', 1500, 'true', '11/01/2020', 1500, '',);
+/* new ExpenseObject ('Housing', 1500, 'true', '11/01/2020', 1500, '',);
 new ExpenseObject ('Food', 900, 'true', '11/03/2020', 1500, '',);
 new ExpenseObject ('Utilities', 200, 'true', '11/06/2020', 300, '',);
 new ExpenseObject ('Loans', 200, 'true', '11/10/2020', 300, '',);
-new ExpenseObject ('Misc', 600, 'true', '11/03/2020', 1509, '',);
+new ExpenseObject ('Misc', 600, 'true', '11/03/2020', 1509, '',); */
 
 // ffcBudget added manually to replicate pushed data
-function CategoryObject (ffcName, ffcBudget) {
+
+/* function CategoryObject (ffcName, ffcBudget) {
   this.categoryName = ffcName;
   this.categoryBudget = ffcBudget;
 
   categoryObjectArray.push(this);
 }
-
+ */
 
 // old category constructor
 /* function CategoryObject (ffcName, ffcType, ffcTrackingStyle) {
@@ -63,7 +158,7 @@ function CategoryObject (ffcName, ffcBudget) {
 } */
 
 
-function defaultCategoryCreator() {
+/* function defaultCategoryCreator() {
   new CategoryObject ('Housing', 1500);
   new CategoryObject ('Food', 1500);
   new CategoryObject ('Utilities', 300);
@@ -73,15 +168,15 @@ function defaultCategoryCreator() {
   return categoryObjectArray;
 }
 
-defaultCategoryCreator();
+defaultCategoryCreator(); */
 
 /* console.log(incomeObjectArray);
 console.log(expenseObjectArray);
 console.log(categoryObjectArray); */
 
-// /////////////////////////////
+
 // End of dummy data samples, can be deleted after LS functionality completed
-// /////////////////////////////
+// ////////////////////////////////////////////////////////////////////
 
 
 
@@ -131,6 +226,10 @@ function chartCategoryDataTotaler() {
     chartCategoryTotalData.push(categoryObjectArray[i].categoryBudget);
   }
 }
+
+/* chartIncomeTotalData = 1000;
+chartCategoryTotalData = 5000; */
+
 
 function budgetOverIncomeAlert() {
   if (chartIncomeTotalData < chartCategoryTotalData) {
